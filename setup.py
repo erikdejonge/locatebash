437069502f838549161e7d873d9308f06387231a
Erik de Jonge <erik@a8.nl>
2016-06-21 14:43:56 +0200

    Tuesday 21 June 2016 (week:25 day:173), 14:43:52

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ license: GNU-GPL2
 """
 from setuptools import setup
 setup(name='locatebash',
-      version='5',
+      version='6',
       description='Combines mdfind and locate',
       url='https://github.com/erikdejonge/locatebash',
       author='Erik de Jonge',
